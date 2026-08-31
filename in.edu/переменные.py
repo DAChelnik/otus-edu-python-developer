56e1bd7ced12ea864e81ff895fee3324383ba6fa
Denis A Chelnik <denis@chelnik.ru>
2026-08-31 16:58:39 +0300

    in.edu

---
+++ in.edu/переменные.py
@@ -0,0 +1,10 @@
+my_color: str = 'Синий'
+sport: str = 'Силовой экстрим'
+name: str = 'Денис'
+
+print ('Меня зовут', name)
+
+print ('Мой любимый цвет:', my_color)
+
+print ('Мой любимый вид спорта:')
+print (sport)
